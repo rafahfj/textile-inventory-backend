bc47c8f26c92aca09980f7f58f29cec8d26ecb1f
Rafah Fajri Juwaeni <rafahfajrijuwaeni@gmail.com>
2025-06-15 13:53:56 +0700

    Update Cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, product, supplier, incoming, outgoing
 import uvicorn
 import os
@@ -11,7 +12,21 @@ app.include_router(supplier.router)
 app.include_router(incoming.router)
 app.include_router(outgoing.router)
 
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+    "https://textile-inventory.hbus.com", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           
+    allow_credentials=True,
+    allow_methods=["*"],             
+    allow_headers=["*"],             
+)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    print(f"--- Starting Uvicorn server on 0.0.0.0:{port} ---") # Baris opsional untuk debugging
+    print(f"--- Starting Uvicorn server on 0.0.0.0:{port} ---")
     uvicorn.run(app, host="0.0.0.0", port=port)
